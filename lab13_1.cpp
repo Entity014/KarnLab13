d68473c7e24e41130e3470c20b76fe4a2c3d540e
Entity014 <radlove.012@gmail.com>
2023-01-10 15:09:15 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,17 +1,32 @@
 #include<iostream>
+#include<ctime>
 
 using namespace std;
 
-int fibonacci(int);
+long long int fibonacci(int);
 
-int main(){
+void Question_2()
+{
     int x;
     cin >> x;
     cout << fibonacci(x);
+}
+
+void Question_3()
+{
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+}
+
+int main(){
+    Question_3();
     return 0; 
 }
 
-int fibonacci(int x)
+long long int fibonacci(int x)
 {
     if (x == 0 || x == 1) return x;
     else return fibonacci(x-1) + fibonacci(x-2);
